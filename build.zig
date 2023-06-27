cdab7b91f75772b93b78dad9e3bb8798240dcba3
sjinzh <99076655+sjinzh@users.noreply.github.com>
2023-06-27 20:52:35 +0800

    update

--- build.zig
+++ build.zig
@@ -1,20 +1,22 @@
 const std = @import("std");
 
-// // Zig Version: 0.11.0-dev.3798+a5e15eced
-// // Zig Build Command: zig build
-// // Zig Run Command: zig build -h
-// //     zig build run_zig_test0
-// //     zig build run_zig_test1
+// Zig Version: 0.11.0-dev.3798+a5e15eced
+// Zig Build Command: zig build
+// Zig Run Command: zig build -h
+//     zig build run_zig_test0
+//     zig build run_zig_test1
+//     zig build run_zig_test2
+//     zig build run_zig_test3
 pub fn build(b: *std.Build) void {
-    // // Standard target options allows the person running `zig build` to choose
-    // // what target to build for. Here we do not override the defaults, which
-    // // means any target is allowed, and the default is native. Other options
-    // // for restricting supported target set are available.
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
 
-    // // Standard optimization options allow the person running `zig build` to select
-    // // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // // set a preferred release mode, allowing the user to decide how to optimize.
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
     // const optimize = .ReleaseFast;
     // const optimize = .ReleaseSmall;
@@ -38,8 +40,8 @@ pub fn build(b: *std.Build) void {
         exe.linkLibC();
         b.installArtifact(exe);
         const run_cmd = b.addRunArtifact(exe);
-        // // This allows the user to pass arguments to the application in the build
-        // // command itself, like this: `zig build run -- arg1 arg2 etc`
+        // This allows the user to pass arguments to the application in the build
+        // command itself, like this: `zig build run -- arg1 arg2 etc`
         run_cmd.step.dependOn(b.getInstallStep());
         if (b.args) |args| run_cmd.addArgs(args);
         const run_step = b.step("run_zig_" ++ name, "Run zig_examples");
@@ -50,6 +52,8 @@ pub fn build(b: *std.Build) void {
     const zig_tests = .{
         "test0",
         "test1",
+        "test2",
+        "test3",
     };
     inline for (zig_tests) |name| {
         const exe = b.addExecutable(.{
@@ -66,8 +70,8 @@ pub fn build(b: *std.Build) void {
         exe.linkLibC();
         b.installArtifact(exe);
         const run_cmd = b.addRunArtifact(exe);
-        // // This allows the user to pass arguments to the application in the build
-        // // command itself, like this: `zig build run -- arg1 arg2 etc`
+        // This allows the user to pass arguments to the application in the build
+        // command itself, like this: `zig build run -- arg1 arg2 etc`
         run_cmd.step.dependOn(b.getInstallStep());
         if (b.args) |args| run_cmd.addArgs(args);
         const run_step = b.step("run_zig_" ++ name, "Run zig_tests");
